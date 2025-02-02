df17024f913a55230f4fe6c61442aa67b63a381a
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-02-02 23:44:43 +0300

    added total price

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import {useState, useRef, useEffect } from 'react'
+import './style/App.css'
+import "./style/reset.css"
+import Header from "./components/Header"
+import EmptyPage from "./components/Empty"
+import Filter from "./components/Filter"
+import NewForm from './components/NewForm'
+import Main from './components/Main'
+import Details from "./components/details"
+
+function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [newData, setNewData] = useState([]);
+  const [showDetails,setShowDetails] = useState(false);
+  const [status, setStatus] = useState("pending"); 
+
+  return (
+    <>
+      <Header />
+      <Filter openForm={() => setShowForm(true)} showForm={showForm} newData={newData} />
+      <div className="container">
+        {showForm ? (
+          <NewForm
+            openForm={() => setShowForm(false)}
+            showForm={showForm}
+            setShowForm = {setShowForm}
+            newData={newData}
+            setNewData={setNewData} 
+            status = {status}
+            setStatus={setStatus}
+          />
+        ) : (
+          <>
+            {newData.length === 0 ? (
+              <EmptyPage />
+            ) : (
+              <Main showDetails = {showDetails} setShowDetails = {setShowDetails} newData={newData}  setNewData={setNewData} status = {status}
+              setStatus={setStatus}  />
+            )}
+          </>
+        )}
+        
+      </div>
+    </>
+  );
+}
+export default App
